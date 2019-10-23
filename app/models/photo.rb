8a33c92fc5ea1a6fa8dcd83cdddc100b5854f41c
ahyoungjo <marcato_07@hotmail.com>
2019-10-23 17:24:14 -0500

    Update photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,3 +4,4 @@ class Photo < ApplicationRecord
   validates :title, :description, :privacy, :uploadDate, :view, :imagePath presence: true
   validates :view, numericality: { only_integer: true }
 end
+
