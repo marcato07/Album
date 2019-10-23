6df9580cd1834dd9ae572470acc9381759d03cbe
ahyoungjo <marcato_07@hotmail.com>
2019-10-23 17:37:57 -0500

    index

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -1,2 +1,4 @@
 class AlbumController < ApplicationController
+	def index
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
-  get 'about/index'
-  get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'about/index'
+  # get 'welcome/index'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+    resources :pages
+
+    get '/static/:permalink', to: 'pages#permalink', as: 'permalink'
+
+    resources :about, only: :index
+    # get /appointments, to: 'appointments#index'
+
+    resources :album, only: %i[index show]
+    # ^ This command creates the same routes as the following two:
+    # get '/houses', to: 'houses#index'
+    # get '/houses/:id', to: 'houses#show', id: /\d+/
+
+    resources :photo, only: :show
+    # get '/students/:id', to: 'students#show', id: /\d+/
+
+    resources :comment, only: :show
+    # get '/teachers/:id', to: 'teachers#show', id: /\d+/
+
+    root to: 'album#index'
+  end
+
 end
